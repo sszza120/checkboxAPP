e309a6c2d5b60e6b1b4332f99298413bf4eb56b3
sszza120 <z875ab@gmail.com>
2020-04-13 10:57:15 +0800

    完成專案0413

--- app/src/main/java/com/sszza120/checkboxapp/MainActivity.java
+++ app/src/main/java/com/sszza120/checkboxapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.sszza120.checkboxapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnOK(View view) {
+
+        CheckBox chk;
+        TextView txvShow;
+        String msg = "";
+
+        int[] id = { R.id.chkMina, R.id.chkMomo, R.id.chkSana, R.id.chkTzuyu };
+
+        for (int i : id){
+            chk = findViewById(i);
+
+            if (chk.isChecked())
+                msg += "\n" + chk.getText();
+        }
+
+        txvShow = findViewById(R.id.txvShow);
+
+        txvShow.setText("我喜歡的歌手有:"+"\n"+msg);
     }
 }
